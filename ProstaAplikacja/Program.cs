cb8c1bb050ac248ec8b74be5590117be84e159d3
NatKoz26 <s26513@pjwstk.edu.pl>
2026-03-30 18:12:21 +0200

    Initial project

---
+++ ProstaAplikacja/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ProstaAplikacja
+{
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("witaj!");
+            //nie mam pomysłu 
+        }
+    }
+
+}
