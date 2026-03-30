2103fb63acdf12d6d337e72475098c1db57c7ab8
NatKoz26 <s26513@pjwstk.edu.pl>
2026-03-30 19:32:00 +0200

    Dodanie metody CalculateAverage(int[]values)

--- ProstaAplikacja/Program.cs
+++ ProstaAplikacja/Program.cs
@@ -60,7 +60,19 @@ namespace ProstaAplikacja
             }
             
         }
-    }
+        static double CalculateAverage(int[] values)
+        {
+            if (values == null || values.Length == 0)
+            throw new ArgumentException("Tablica nie może być pusta.");
 
-    
-}
+            int suma = 0;
+
+             foreach (int v in values)
+            {
+                 suma += v;
+             }
+
+            return (double)suma / values.Length;
+        }
+    }
+    }
