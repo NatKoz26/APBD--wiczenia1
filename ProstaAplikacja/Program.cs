cfe334bb6d8a25778fbdb31a530529faecd5cc6a
NatKoz26 <s26513@pjwstk.edu.pl>
2026-03-30 18:32:11 +0200

    Nowa Funkcjonalnosc: Sprawdzenie poprawnosci imienia

--- ProstaAplikacja/Program.cs
+++ ProstaAplikacja/Program.cs
@@ -7,11 +7,41 @@ namespace ProstaAplikacja
             Console.WriteLine("witaj!");
             
             //Pierwsza funkcjonalność
-            Console.Write("Podaj swoje imie: ");
-            string imie = Console.ReadLine();
+
+
+            string imie = PobierzISprawdzImie();
+            Console.WriteLine($"Cześć, {imie}!");
 
             
         }
+
+        //funkcje sprawdzenia poprawności: nie chcemy cyfr w imieniu 
+        static bool PoprawneImie(string imie)
+        {
+            foreach(char znak in imie)
+            {
+                if(char.IsDigit(znak))
+                {return false;}
+
+            }
+            return !string.IsNullOrWhiteSpace(imie);
+        }
+         static string PobierzISprawdzImie()
+        {
+            while (true)
+            {
+                Console.Write("Podaj swoje imię: ");
+                string imie = Console.ReadLine();
+
+                if (PoprawneImie(imie))
+                {
+                    return imie;
+                }
+
+                Console.WriteLine("Błąd: imię nie może zawierać cyfr. Spróbuj ponownie.\n");
+            }
+        }
     }
 
+    
 }
